53eef1ed84a909ae60e7e73c5f3098a78ccdd872
tnsgud <qkrtnsgud0229@gmail.com>
2023-09-24 16:01:35 +0900

    add go_router library & using MVVM design pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
+import 'package:final_project/views/home_screen.dart';
+import 'package:final_project/views/sign_in_screen.dart';
+import 'package:final_project/views/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/sign-in',
+      builder: (context, state) => const SignInScreen(),
+    ),
+    GoRoute(
+      path: '/sign-up',
+      builder: (context, state) => const SignUpScreen(),
+    ),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    )
+  ],
+);
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -10,12 +31,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Nomad Flutter Final',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      routerConfig: router,
     );
   }
 }
