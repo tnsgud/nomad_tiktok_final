bee4562f42e093e5bab0bad29a98ed3c209b3153
tnsgud <qkrtnsgud0229@gmail.com>
2023-09-24 16:44:53 +0900

    insert firebase init code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:final_project/firebase_options.dart';
 import 'package:final_project/views/home_screen.dart';
 import 'package:final_project/views/sign_in_screen.dart';
 import 'package:final_project/views/sign_up_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -22,7 +24,12 @@ final router = GoRouter(
   ],
 );
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
